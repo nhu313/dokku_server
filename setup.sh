2feb8e4af955a6d508e1d13b441a350afe41c707
Nhu Nguyen <nhu313@gmail.com>
2016-01-29 20:31:32 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,4 @@ dokku config:set qa BUILDPACK_URL=https://github.com/ddollar/heroku-buildpack-mu
 dokku config:set qa LC_ALL=en_US.utf8
 sudo dokku plugin:install https://github.com/dokku/dokku-postgres.git
 dokku postgres:create qa_test
-sudo dokku plugin:install https://github.com/dokku/dokku-letsencrypt.git
-dokku letsencrypt:email qa nhu313@gmail.com
-dokku letsencrypt qa
+dokku postgres:link qa_test qa
