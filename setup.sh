2b9532552334f29053f2d7bf07b3c5900744602e
Nhu Nguyen <nhu313@gmail.com>
2016-02-17 15:22:12 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,6 @@
-dokku apps:create qa
-dokku config:set qa BUILDPACK_URL=https://github.com/ddollar/heroku-buildpack-multi.git
-dokku config:set qa LC_ALL=en_US.utf8
+dokku apps:create app_name
+dokku config:set app_name BUILDPACK_URL=https://github.com/ddollar/heroku-buildpack-multi.git
+dokku config:set app_name LC_ALL=en_US.utf8
 sudo dokku plugin:install https://github.com/dokku/dokku-postgres.git
-dokku postgres:create qa_test
-dokku postgres:link qa_test qa
-
-
-dokku apps:create prod
-dokku config:set prod BUILDPACK_URL=https://github.com/ddollar/heroku-buildpack-multi.git
-dokku config:set prod LC_ALL=en_US.utf8
-sudo dokku plugin:install https://github.com/dokku/dokku-postgres.git
-dokku postgres:create mp_prod
-dokku postgres:link mp_prod prod
+dokku postgres:create db_name
+dokku postgres:link db_name app_name
